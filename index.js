39fabb57b9bfa7838e4e71e08addf6c15239ef9e
gonzalezbri <4717bg@gmail.com>
2023-04-23 10:35:55 -0700

    fixed 404

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('*',(req,res)=>{
-    res.status.apply(404).send('<h1>404 Page</h1>')
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 app.listen(process.env.PORT)
