82086d4a11cd0026ecbababc53adda9e8f3b0928
gonzalezbri <4717bg@gmail.com>
2023-04-25 20:01:18 -0700

    adding the style page

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set('view engine','jsx')
-app.engine('jsx',require('express-react-views').createEngine())
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-app.use('/places',require('./controllers/places'))
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -13,6 +18,8 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => {
     res.render('error404')
-    })
+})
 
+// Listen for Connections
 app.listen(process.env.PORT)
+
