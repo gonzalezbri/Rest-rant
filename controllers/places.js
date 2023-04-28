d0067a2bfa56a6c4feba507f0013fefbab8a1412
gonzalezbri <4717bg@gmail.com>
2023-04-27 17:40:16 -0700

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,24 @@
 const router = require('express').Router()
 
+
 router.get('/',(req,res)=>{
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/thai.jpg'
     }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/catcafe.jpg'
     }]
     res.render('places/index',{places})
 })
+router.get('/new', (req, res) => {
+    res.render('places/new')
+});
 
 module.exports = router
--- views/home.jsx
+++ views/home.jsx
@@ -6,9 +6,17 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+        <div>
+            <img width="500px" src="/images/home.jpg" alt="restaurant" />
+        <div>
+        Photo by <a href="https://unsplash.com/@vardarious">Volkan Vardar</a> on <a href="https://unsplash.com/photos/1H30uRC1plc">Unsplash</a>
+        </div>
+        </div>
             <a href="/places">
-<button className="btn-primary">Places Page</button>
-</a>
+                <button className="btn-primary">Places Page</button>
+    </a>
+
+
 
         </main>
     </Def>
