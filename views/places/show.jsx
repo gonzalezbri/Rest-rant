a7fbfa0bd6f458cc93cfc9bf4fb9b56f84a48d60
gonzalezbri <4717bg@gmail.com>
2023-04-30 14:16:31 -0700

    added the edit functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,15 @@ function show (data) {
         <Def>
             <main>
             <h1>{ data.place.name }</h1>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+    Edit
+</a>     
+
+<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+    <button type="submit" className="btn btn-danger">
+    Delete
+    </button>
+</form>
             <h2>Rating</h2>
             <h2>Description</h2>
             <h3>Comments</h3>
